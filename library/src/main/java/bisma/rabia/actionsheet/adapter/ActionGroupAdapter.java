d89b4e7c428d1c0b7e3dac02ea165d5949a33d14
rabismail <rabismail@hotech.systems>
2020-04-16 03:25:20 +0300

    added a check for duplicate groups (groups with the same title), if any is found merge the new group's actions with the old ones. added a parameter to bypass this check.

--- library/src/main/java/bisma/rabia/actionsheet/adapter/ActionGroupAdapter.java
+++ library/src/main/java/bisma/rabia/actionsheet/adapter/ActionGroupAdapter.java
@@ -42,7 +42,7 @@ public class ActionGroupAdapter extends RecyclerView.Adapter<ActionGroupAdapter.
     @Override
     public void onBindViewHolder(@NonNull ActionGroupHolder holder, int position) {
         final ActionGroup actionGroup = mGroupedActions.get(position);
-        holder.mTitle.setText(actionGroup.getaTitle());
+        holder.mTitle.setText(actionGroup.getTitle());
         // filter only visible actions
         List<Action> actions = actionGroup.getActions();
         actions = Stream.ofNullable(actions).filter(action -> Utils.isObjectNull(action.isVisible()) || action.isVisible()).toList();
@@ -58,18 +58,41 @@ public class ActionGroupAdapter extends RecyclerView.Adapter<ActionGroupAdapter.
     }
 
     /**
-     * @param aActionGroup {@link ActionGroup} object
+     * @param aActionGroup            {@link ActionGroup} object
+     * @param aCheckForDuplicateTitle true to check for existing titles and merge their actions, otherwise add aActionGroups without any further check.
      */
-    public void addItem(ActionGroup aActionGroup) {
-        mGroupedActions.add(aActionGroup);
+    public void addItem(ActionGroup aActionGroup, boolean aCheckForDuplicateTitle) {
+        if (aCheckForDuplicateTitle) {
+            Stream.of(mGroupedActions).filter(value -> value.getTitle().equals(aActionGroup.getTitle())).findSingle().ifPresentOrElse(group -> {
+                final int index = mGroupedActions.indexOf(group);
+                group.getActions().addAll(aActionGroup.getActions());
+                mGroupedActions.set(index, group);
+            }, () -> mGroupedActions.add(aActionGroup));
+        }
+        else {
+            mGroupedActions.add(aActionGroup);
+        }
         notifyDataSetChanged();
     }
 
     /**
      * @param aActionGroups list of {@link ActionGroup} object
+     * @param aCheckForDuplicateTitle true to check for existing titles and merge their actions, otherwise add aActionGroups without any further check.
      */
-    public void addItems(List<ActionGroup> aActionGroups) {
-        mGroupedActions.addAll(aActionGroups);
+    public void addItems(List<ActionGroup> aActionGroups, boolean aCheckForDuplicateTitle) {
+        if (aCheckForDuplicateTitle) {
+            Stream.of(aActionGroups).forEach(aActionGroup -> {
+                // check for existence...
+                Stream.ofNullable(mGroupedActions).filter(value -> aActionGroup.getTitle().equals(value.getTitle())).findSingle().ifPresentOrElse(group -> {
+                    final int index = mGroupedActions.indexOf(group);
+                    group.getActions().addAll(aActionGroup.getActions());
+                    mGroupedActions.set(index, group);
+                }, () -> mGroupedActions.add(aActionGroup));
+            });
+        }
+        else {
+            mGroupedActions.addAll(aActionGroups);
+        }
         notifyDataSetChanged();
     }
 
--- library/src/main/java/bisma/rabia/actionsheet/model/ActionGroup.java
+++ library/src/main/java/bisma/rabia/actionsheet/model/ActionGroup.java
@@ -1,26 +1,33 @@
 package bisma.rabia.actionsheet.model;
 
-
 import java.util.*;
 
 import androidx.annotation.NonNull;
 
 public class ActionGroup {
-    private String aTitle;
+    private String mTitle;
     private List<Action> mActions;
     private boolean isHeightCalculated;
 
-    public ActionGroup(String aATitle, List<Action> aActions) {
-        aTitle = aATitle;
+    public ActionGroup(String aTitle, List<Action> aActions) {
+        mTitle = aTitle;
         mActions = aActions;
     }
 
-    public String getaTitle() {
-        return aTitle;
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public void setTitle(String aTitle) {
+        mTitle = aTitle;
     }
 
     public List<Action> getActions() {
         return mActions;
+    }
+
+    public void setActions(List<Action> aActions) {
+        mActions = aActions;
     }
 
     public boolean isHeightCalculated() {
@@ -36,21 +43,23 @@ public class ActionGroup {
         if (this == aO) return true;
         if (!(aO instanceof ActionGroup)) return false;
         ActionGroup that = (ActionGroup) aO;
-        return Objects.equals(getaTitle(), that.getaTitle()) &&
+        return isHeightCalculated() == that.isHeightCalculated() &&
+                Objects.equals(getTitle(), that.getTitle()) &&
                 Objects.equals(getActions(), that.getActions());
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(getaTitle(), getActions());
+        return Objects.hash(getTitle(), getActions(), isHeightCalculated());
     }
 
     @NonNull
     @Override
     public String toString() {
         return "ActionGroup{" +
-                "aTitle='" + aTitle + '\'' +
+                "mTitle='" + mTitle + '\'' +
                 ", mActions=" + mActions +
+                ", isHeightCalculated=" + isHeightCalculated +
                 '}';
     }
 }
